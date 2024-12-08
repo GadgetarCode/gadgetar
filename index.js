f096ee8f71c5aa648e2be908c8e489bcda708165
GadgetarCode <gadgetarcode@gmail.com>
2024-12-08 21:01:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import { WebflowClient } from "webflow-api";
+// import { WebflowClient } from "webflow-api";
 import express from "express";
 import axios from "axios";
 const PORT = process.env.PORT || 5500;
 const app = express();
 
-const client = new WebflowClient({
-accessToken: "d11f82236d18264c45d33fda2857f04c96e14771134529aac94c9fb491b5dbcb",
-});
-const siteID = '66fd1c590193b201914b0d7c';
+// const client = new WebflowClient({
+// accessToken: "d11f82236d18264c45d33fda2857f04c96e14771134529aac94c9fb491b5dbcb",
+// });
+// const siteID = '66fd1c590193b201914b0d7c';
 
 app.use(
   express.json({
@@ -27,33 +27,38 @@ app.use(function (req, res, next) {
     next();
   });
 
-
-app.post("/find-products", async (req, res) => {
+app.post("/find-products", (req, res) => {
   const { skus } = req.body;
   if (!Array.isArray(skus)) {
     return res.status(400).json({ error: "Invalid data format. 'skus' must be an array." });
   }
 
-  try {
-    const allProducts = await client.items.list({ siteID });
-    const matchingProducts = allProducts.items.filter((product) =>
-      skus.includes(product.sku)
-    );
+  const options = {
+    method: 'GET',
+    url: 'https://api.webflow.com/v2/sites/66fd1c590193b201914b0d7c/products',
+    headers: {
+      accept: 'application/json',
+      authorization: 'Bearer d11f82236d18264c45d33fda2857f04c96e14771134529aac94c9fb491b5dbcb',
+    }
+  };
 
-    const response = matchingProducts.map((product) => ({
-      name: product.name,
-      slug: product.slug,
-    }));
-
-    res.json(response);
-  } catch (error) {
-    console.error("Error fetching products:", error);
-    res.status(500).json({ error: "Internal server error." });
-  }
+  axios
+    .request(options)
+    .then(function (response) {
+      const allProducts = response.data.items;
+      const matchingProducts = allProducts.filter((product) => 
+        skus.includes(product.sku)
+      );
+      const responseData = matchingProducts.map((product) => ({
+        name: product.name,
+        slug: product.slug,
+      }));
+      res.json(responseData);
+    })
+    .catch(function (error) {
+      console.error("Error fetching products:", error);
+      res.status(500).json({ error: "Internal server error." });
+    });
 });
-
-
-
-
 
 app.listen(PORT, () => console.log("Server on " + PORT))
