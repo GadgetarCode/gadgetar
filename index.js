409d7615ae6b426b2e95a69222af0dabee615b3d
GadgetarCode <gadgetarcode@gmail.com>
2024-12-08 17:58:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
 );
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://gadgetar.com.ua");
+    res.header("Access-Control-Allow-Origin", "https://www.gadgetar.com.ua");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
     res.header(
       "Access-Control-Allow-Headers",
